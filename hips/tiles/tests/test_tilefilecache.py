0baa2aaebbc20193620539fccea271c6083a6bd8
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-06-04 12:33:06 +0500

    Add hips.tiles sub-module

---
+++ hips/tiles/tests/test_tilefilecache.py
@@ -0,0 +1,10 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from ..tilefilecache import *
+
+
+def test_read_file_properties():
+    return
+
+
+def test_load_tiles():
+    return
---
+++ hips/tiles/tilefilecache.py
@@ -0,0 +1,39 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""HiPS tile cache class.
+
+TODO: link to description of the method, and implement!
+"""
+
+__all__ = [
+    'read_file_properties', 'load_tiles'
+]
+
+
+def read_file_properties(self):
+    """Read properties of files from a given directory!
+
+    Given a directory, this method returns the properties
+    of a file within that directory
+
+    Parameters
+    ----------
+    directory : str
+        Relative path to the directory
+
+    Returns
+    -------
+    file_properties : list
+        Properties of the file
+    """
+    return
+
+
+def load_tiles(self):
+    """Loads HiPS tiles from cache!
+
+    Returns
+    -------
+    tiles : np.array
+        A matrix contain HiPS tiles
+    """
+    return
